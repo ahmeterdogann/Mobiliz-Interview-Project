6d28fdbd18d9c625b13258b581b703c8e2dcbd6d
ahmeterdogann <ahmett.erdogan.23@gmail.com>
2023-08-10 05:41:51 +0300

    api-gateaway

---
+++ api-gateaway/src/main/java/com/ahmeterdogan/ApiGateawayApplication.java
@@ -0,0 +1,13 @@
+package com.ahmeterdogan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateawayApplication.class, args);
+	}
+
+}
---
+++ api-gateaway/src/test/java/com/ahmeterdogan/ApiGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmeterdogan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
