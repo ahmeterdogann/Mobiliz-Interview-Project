220172c090b8944941da8e374cdebf7655440516
ahmeterdogann <ahmett.erdogan.23@gmail.com>
2023-08-12 20:32:13 +0300

    idea module settings

---
+++ company-service/src/main/java/com/ahmeterdogan/CompanyServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ahmeterdogan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class CompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+
+}
